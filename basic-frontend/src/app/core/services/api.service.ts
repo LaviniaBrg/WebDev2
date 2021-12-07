b7ef3d1f26dda8497016b642169317eb32b39c09
Yoshi <s.ladner@komplexworld.com>
2021-12-07 16:29:40 +0100

    Verlinkung funktioniert wieder

--- basic-frontend/src/app/core/services/api.service.ts
+++ basic-frontend/src/app/core/services/api.service.ts
@@ -1,8 +1,14 @@
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {CreateEchoInput, Echo} from '../../models/echo.model';
+import {catchError} from 'rxjs/operators';
 import {Artikel} from "../../models/artikel.model";
 import {Kunde} from "../../models/kunde.model";
 
-
+@Injectable({
+  providedIn: 'root'
+})
 export class ApiService {
 
   private readonly baseUrl = 'http://localhost:3000/api';
@@ -16,5 +22,39 @@ export class ApiService {
 
   getKunden() {
     return this.http.get<Kunde[]>(`${this.baseUrl}/kunden`)
+  }
+  createEcho(echo: CreateEchoInput): Observable<Echo> {
+    return this.http.post<Echo>(
+      `${this.baseUrl}/echo`,
+      echo
+    ).pipe(
+      catchError((err) => {
+        console.log('In Service:', err);
+        return throwError(err);
+      })
+    );
+  }
+
+  doError(): Observable<Echo> {
+    return this.http.post<Echo>(
+      `${this.baseUrl}/echo`,
+      {}
+    ).pipe(
+      catchError((err) => {
+        console.log('In Service:', err);
+        return throwError(err);
+      })
+    );
+  }
+
+  getEchos(contains?: string): Observable<Echo[]> {
+    return this.http.get<Echo[]>(
+      `${this.baseUrl}/echo`,
+      {
+        params: contains ? {
+          contains
+        } : undefined
+      }
+    );
   }
 }
